90ad00b92110a6c4760a9c93e8c399a0f561e99c
Toprak <topraktas19@hotmail.com>
2025-09-07 23:23:52 +0300

    page updates

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,13 +4,15 @@ import {
 } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
-import ArticlesListPage from './pages/ArticlesList'
+import ArticlesListPage from './pages/ArticlesListPage'
 import AboutPage from './pages/AboutPage'
 import ArticlePage from './pages/ArticlePage'
 import Layout from './Layout'
+import NotFoundPage from './pages/NotFoundPage'
 const routes = [{
   path: '/',
   element: <Layout />,
+  errorElement: <NotFoundPage />,
   children: [
     {
       path: '/',
