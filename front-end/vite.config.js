ccb742de5757e6c39999663c74f82bc0c703d733
Toprak <topraktas19@hotmail.com>
2025-10-25 21:13:24 +0300

    proxy and axios added

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://4n7m63s3-8000.euw.devtunnels.ms',
+        changeOrigin: true,
+      }
+    }
+  }
+});
