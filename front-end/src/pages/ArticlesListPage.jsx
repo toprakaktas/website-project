287d0356b89b8da86a79fe7b7039560d886f84e3
Toprak <topraktas19@hotmail.com>
2025-10-25 21:13:45 +0300

    unused part removed

--- front-end/src/pages/ArticlesListPage.jsx
+++ front-end/src/pages/ArticlesListPage.jsx
@@ -3,7 +3,6 @@ import ArticlesList from "../ArticlesList";
 export default function ArticlesListPage() {
     return (
         <>
-            <h1>Articles</h1>
             <ArticlesList articles={articles} />
         </>
     );
