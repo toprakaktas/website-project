c9b194be4464cd5e0fffee69135c8e8f48b96622
Toprak <topraktas19@hotmail.com>
2025-10-28 23:54:00 +0300

    login and sign up added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -9,6 +9,8 @@ import AboutPage from './pages/AboutPage'
 import ArticlePage, { loader as articleLoader } from './pages/ArticlePage'
 import Layout from './Layout'
 import NotFoundPage from './pages/NotFoundPage'
+import LoginPage from './pages/LoginPage'
+import CreateAccountPage from './pages/CreateAccountPage'
 
 const routes = [{
   path: '/',
@@ -28,6 +30,12 @@ const routes = [{
       path: '/articles/:name', // -> /articles/learn-react
       element: <ArticlePage />,
       loader: articleLoader
+    }, {
+      path: '/login',
+      element: <LoginPage />
+    }, {
+      path: '/create-account',
+      element: <CreateAccountPage />
     }]
 }]
 const router = createBrowserRouter(routes);
