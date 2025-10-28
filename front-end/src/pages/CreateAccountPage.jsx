2d7aa79634f602e993f0f00999ac58b3fd21ab0f
Toprak <topraktas19@hotmail.com>
2025-10-28 23:28:30 +0300

    sign up page created

---
+++ front-end/src/pages/CreateAccountPage.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+export default function CreateAccountPage() {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ confirmPassword, setConfirmPassword ] = useState('');
+    const [ error, setError ] = useState('');
+
+    const navigate = useNavigate();
+
+    async function createAccount() {
+        if (password != confirmPassword) {
+            setError('Passwords do not match!');
+            return;
+        }
+        try {
+            await createUserWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+
+    return (
+        <>
+            <h1>Create Account</h1>
+            {error && <p>{error}</p>}
+            <input 
+                placeholder='Your email address'
+                value={email}
+                onChange={ e => setEmail(e.target.value) } />
+            <input 
+                placeholder='Your password'
+                type='password'
+                value={password}
+                onChange={ e => setPassword(e.target.value) } />
+            <input 
+                placeholder='Confirm password'
+                type='password'
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)} />
+            <button onClick={createAccount}>Create Account</button>
+
+            <Link to='/login'>Already have an account? Log in here</Link>
+
+        </>
+    );
+}
